4871b4cc310c5130a45226646018e216435a85be
meong92 <38969204+meong92@users.noreply.github.com>
2024-05-07 10:45:57 +0700

    up

--- query.sql
+++ query.sql
@@ -1,6 +1,7 @@
 -- user:
 -- su, editor, asisten
 
+-- CREATE DATABASE norman_log;
 
 CREATE TABLE IF NOT EXISTS users( 
     id SERIAL PRIMARY KEY, 
@@ -24,6 +25,7 @@ CREATE TABLE IF NOT EXISTS client(
     file_scan_ktp VARCHAR
 );
 
+
 CREATE TABLE IF NOT EXISTS perkara(
    id SERIAL PRIMARY KEY,
    id_client SERIAL,
@@ -32,17 +34,24 @@ CREATE TABLE IF NOT EXISTS perkara(
    judul VARCHAR(100),
    deskripsi VARCHAR,
    lokasi VARCHAR(100),
-   nm_pengugat VARCHAR(100),
+   nm_penggugat VARCHAR(100),
    nm_tergugat VARCHAR(100),
-   tgl_mulai DATE NOT NULL DEFAULT CURRENT_DATE,
-   tgl_selesai DATE NOT NULL DEFAULT CURRENT_DATE
+   tgl_dibuat_perkara TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+   tgl_selesai_perkara TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS log_activity( 
     id SERIAL PRIMARY KEY, 
-    id_perkara SERIAL, 
-    id_client SERIAL, 
-    id_asisten SERIAL, 
+    no_perkara VARCHAR(100), 
+    id_client VARCHAR(100), 
+    id_asisten VARCHAR(100), 
     log_text VARCHAR,
     log_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP 
 );
+
+
+-- CREATE TABLE test_table (
+--     id SERIAL PRIMARY KEY,
+--     nm_penggugat VARCHAR(100),
+--     your_column_name TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+-- );
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const cors = require("cors");
 const { Pool } = require('pg');
-const PORT = 8080;
+const PORT = 8082;
 
 app.use(cors());
 
@@ -16,6 +16,49 @@ const pool = new Pool({
   database: 'norman_log',
   // password: 'your_password',
   port: 5432, // Default PostgreSQL port
+});
+
+app.get('/get_perkara', async (req, res) => {
+  try {
+    const client = await pool.connect();
+    // const result = await client.query('SELECT  jns_perkara FROM perkara ORDER BY tgl_dibuat_perkara DESC');
+    const result = await client.query('SELECT id, id_client, no_perkara, jns_perkara, judul, deskripsi, tgl_dibuat_perkara FROM perkara ORDER BY tgl_dibuat_perkara DESC');
+    client.release(); // Release the client back to the pool
+    res.status(200).json(result.rows);
+  } 
+  catch (err) {
+    console.error('Error executing query', err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+app.get('/get_log/:perkara_id', async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query("SELECT * FROM log_activity WHERE no_perkara='"+req.params.perkara_id+"'  ORDER BY log_time DESC");
+    client.release(); // Release the client back to the pool
+    res.status(200).json(result.rows);
+  } 
+  catch (err) {
+    console.error('Error executing query', err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+app.post('/add_log', async (req, res) => {
+  const { no_perkara, log_text } = req.body;
+  
+  try {
+    const client = await pool.connect();
+    const result = await client.query("INSERT INTO log_activity (no_perkara, log_text) VALUES ('"+no_perkara+"', '"+log_text+"')");
+    client.release(); // Release the client back to the pool
+    res.status(200).json({ success: true });
+
+  } catch (err) {
+    console.error('Error executing query', err);
+    res.status(500).json({ error: 'Something wrong' });
+  }
+  
 });
 
 app.post('/login', async (req, res) => {
